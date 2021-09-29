2316d00754fa0fbf7db2a24882d44a0c02be8e9e
Toni <toni@archPC.localdomain>
2021-09-29 23:04:56 +0200

    New site

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
-const toggleButton = document.querySelector(".toggle-button");
-const navbarLinks = document.querySelector(".navbar-links");
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+const navbar = document.querySelector(".navbar");
 
-toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  burger.classList.toggle("active");
+});
+
+navLinks.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  burger.classList.toggle("active");
+});
+
+window.addEventListener("scroll", () => {
+  const lastPosition = window.scrollY;
+  if (lastPosition > 50) {
+    navbar.classList.add("nav-active");
+  } else if (navbar.classList.contains("nav-active")) {
+    navbar.classList.remove("nav-active");
+  } else {
+    navbar.classList.remove("nav-active");
+  }
 });
